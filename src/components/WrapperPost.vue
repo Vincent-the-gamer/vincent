d0f36647938f06ce7f280467411661e0aa878c72
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-02-14 13:54:48 +0800

    chore: update.

--- src/components/WrapperPost.vue
+++ src/components/WrapperPost.vue
@@ -14,14 +14,7 @@ const content = ref<HTMLDivElement>()
 
 function formatDateString(dateString: string): string {
   const date = new Date(dateString)
-  const year = date.getUTCFullYear()
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0') // 月份从 0 开始，所以加 1
-  const day = String(date.getUTCDate()).padStart(2, '0')
-  const hours = String(date.getUTCHours()).padStart(2, '0')
-  const minutes = String(date.getUTCMinutes()).padStart(2, '0')
-  const seconds = String(date.getUTCSeconds()).padStart(2, '0')
-
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+  return date.toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' })
 }
 
 onMounted(() => {
