7748d26a39e2dc257eb0fb1d2a4b044405421a7d
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-04-11 15:58:53 +0800

    feat: integrate typed.js

---
+++ src/components/Typewriter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div ref="typedEl" inline-block />
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from "vue"
+import Typed from "typed.js"
+
+const props = defineProps<{
+    strings: string[]
+}>()
+
+const typedEl = ref<HTMLElement>(null)
+
+let typed: any = null
+
+onMounted(() => {
+    typed = new Typed(typedEl.value, {
+        strings: props.strings,
+        typeSpeed: 100,
+        backSpeed: 60,
+        typeDelay: 500,
+        backDelay: 500,
+        loop: true
+    })
+})
+
+onUnmounted(() => {
+    if(typed) {
+        typed.destroy()
+    }
+})
+
+</script>
