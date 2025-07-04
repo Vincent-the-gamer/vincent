6652b90ead1d3023958a3e8ca710ea020f395666
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-07-04 09:41:47 +0800

    feat: add live2d girl.

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ import autoRoutes from 'pages-generated'
 import { ViteSSG } from 'vite-ssg'
 import { setupRouterScroller } from 'vue-router-better-scroller'
 import App from './App.vue'
+
 import '@unocss/reset/tailwind.css'
-
 import 'floating-vue/dist/style.css'
-
 import 'markdown-it-github-alerts/styles/github-colors-light.css'
 import 'markdown-it-github-alerts/styles/github-colors-dark-class.css'
 import 'markdown-it-github-alerts/styles/github-base.css'
