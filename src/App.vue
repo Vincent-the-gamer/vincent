a2eed6d30029983496ab28abd245bd810514a2ad
Vincent-the-gamer <erxianqiaottjt@163.com>
2026-01-01 23:58:41 +0800

    chore(zed): use global config.

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,90 @@
 <script setup lang="ts">
-const title = useTitle()
-const defaultTitle = title.value
+const title = useTitle();
+const defaultTitle = title.value;
 
-const isLeft = usePageLeave()
-watch(() => isLeft.value, (newVal) => {
-  newVal ? title.value = '不要离开人家嘛 ヽ(。>д<)ｐ' : title.value = defaultTitle
-})
+const isLeft = usePageLeave();
+watch(
+    () => isLeft.value,
+    (newVal) => {
+        newVal
+            ? (title.value = "不要离开人家嘛 ヽ(。>д<)ｐ")
+            : (title.value = defaultTitle);
+    },
+);
 
-const route = useRoute()
+const route = useRoute();
 
-const imageModel = ref<HTMLImageElement>()
+const imageModel = ref<HTMLImageElement>();
 
-useEventListener('click', async (e) => {
-  const path = Array.from(e.composedPath())
-  const first = path[0]
-  if (!(first instanceof HTMLElement))
-    return
-  if (first.tagName !== 'IMG')
-    return
-  if (first.classList.contains('no-preview'))
-    return
-  if (path.some(el => el instanceof HTMLElement && ['A', 'BUTTON'].includes(el.tagName)))
-    return
-  if (!path.some(el => el instanceof HTMLElement && el.classList.contains('prose')))
-    return
+useEventListener("click", async (e) => {
+    const path = Array.from(e.composedPath());
+    const first = path[0];
+    if (!(first instanceof HTMLElement)) return;
+    if (first.tagName !== "IMG") return;
+    if (first.classList.contains("no-preview")) return;
+    if (
+        path.some(
+            (el) =>
+                el instanceof HTMLElement &&
+                ["A", "BUTTON"].includes(el.tagName),
+        )
+    )
+        return;
+    if (
+        !path.some(
+            (el) => el instanceof HTMLElement && el.classList.contains("prose"),
+        )
+    )
+        return;
 
-  // Do not open image when they are moving. Mainly for mobile to avoid conflict with hovering behavior.
-  const pos = first.getBoundingClientRect()
-  await new Promise(resolve => setTimeout(resolve, 50))
-  const newPos = first.getBoundingClientRect()
-  if (pos.left !== newPos.left || pos.top !== newPos.top)
-    return
+    // Do not open image when they are moving. Mainly for mobile to avoid conflict with hovering behavior.
+    const pos = first.getBoundingClientRect();
+    await new Promise((resolve) => setTimeout(resolve, 50));
+    const newPos = first.getBoundingClientRect();
+    if (pos.left !== newPos.left || pos.top !== newPos.top) return;
 
-  imageModel.value = first as HTMLImageElement
-})
+    imageModel.value = first as HTMLImageElement;
+});
 
-onKeyStroke('Escape', (e) => {
-  if (imageModel.value) {
-    imageModel.value = undefined
-    e.preventDefault()
-  }
-})
-
+onKeyStroke("Escape", (e) => {
+    if (imageModel.value) {
+        imageModel.value = undefined;
+        e.preventDefault();
+    }
+});
 </script>
 
 <template>
-  <NavBar />
-  <main class="px-7 py-10 of-x-hidden">
-    <RouterView />
-    <Footer :key="route.path" />
-  </main>
-  <Transition name="fade">
-    <div v-if="imageModel" fixed top-0 left-0 right-0 bottom-0 z-500 backdrop-blur-7 @click="imageModel = undefined">
-      <div absolute top-0 left-0 right-0 bottom-0 bg-black:30 z--1 />
-      <img :src="imageModel.src" :alt="imageModel.alt" w-full h-full object-contain>
-    </div>
-  </Transition>
-  <FantasyLand />
-  <ClientOnly>
-    <!-- music player, client only -->
-    <APlayer />
-  </ClientOnly>
+    <NavBar />
+    <main class="px-7 py-10 of-x-hidden">
+        <RouterView />
+        <Footer :key="route.path" />
+    </main>
+    <Transition name="fade">
+        <div
+            v-if="imageModel"
+            fixed
+            top-0
+            left-0
+            right-0
+            bottom-0
+            z-500
+            backdrop-blur-7
+            @click="imageModel = undefined"
+        >
+            <div absolute top-0 left-0 right-0 bottom-0 bg-black:30 z--1 />
+            <img
+                :src="imageModel.src"
+                :alt="imageModel.alt"
+                w-full
+                h-full
+                object-contain
+            />
+        </div>
+    </Transition>
+    <FantasyLand />
+    <ClientOnly>
+        <!-- music player, client only -->
+        <APlayer />
+    </ClientOnly>
 </template>
